1e0633ff57cd97a10fee5aee1fb7c66990d0b86a
wangxuguang <nova_wangxuguang@126.com>
2018-11-08 18:02:24 +0800

    1. 增加fragment布局

--- app/src/main/java/wxgaly/android/imdemo/home/HomeActivity.kt
+++ app/src/main/java/wxgaly/android/imdemo/home/HomeActivity.kt
@@ -1,7 +1,11 @@
 package wxgaly.android.imdemo.home
 
 import android.os.Bundle
+import android.support.v4.view.PagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import kotlinx.android.synthetic.main.activity_home.*
 import wxgaly.android.imdemo.R
 
 /**
@@ -15,6 +19,21 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        initView()
+
+    }
+
+    private fun initView() {
+
+
+        vp.adapter = object : PagerAdapter() {
+            override fun isViewFromObject(p0: View, p1: Any): Boolean = p0 == p1
+
+            override fun getCount(): Int {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        }
     }
 
 }
